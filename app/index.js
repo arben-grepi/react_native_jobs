51bcbf34966206869b56c5e8f10ee91424ca4078
arben-systems3000 <AGrepi@systems3000.com>
2025-03-03 21:34:50 +0100

    added paths to the components and constants

--- app/index.js
+++ app/index.js
@@ -1,4 +1,14 @@
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView } from "react-native";
 import { View, Text } from "react-native";
+
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 const Home = () => {
   return (
